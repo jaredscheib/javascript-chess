ea01ba88a3b8194f6c6951d8b238f5410e0ca50c
jaredscheib <jared.scheib@gmail.com>
2015-12-23 23:30:37 -0800

    (fix) Connect HTML to js

--- chess.js
+++ chess.js
@@ -1,5 +1,5 @@
-document.on('load', function (){
-  document.getElementById('display', function(item) {
-    item.innerHTML = "<span>hello world!</span>"
-  });
+document.addEventListener('DOMContentLoaded', function (event){
+  var display = document.getElementById('display');
+  console.log(display);
+  display.innerHTML = "<span>hello world!</span>";
 });
