c4ef8d098981d2869ee21beecb89276d39fee9b2
jaredscheib <jared.scheib@gmail.com>
2015-12-24 01:32:06 -0800

    (feat) Add first unicode chess piece

--- chess.js
+++ chess.js
@@ -2,45 +2,73 @@ document.addEventListener('DOMContentLoaded', function (event){
   var canvas = document.getElementById('canvas');
   canvas.innerHTML = "<span>Native JavaScript Chess</span>";
 
-  var generateBoard = function(width, height, parent) {
-    width = width || 8;
-    height = height || 8;
-    parent = parent || document.body;
+  var board = generateBoard();
+  var pieces = placePieces(board);
+});
 
-    var board = document.createElement('div');
-    board.id = 'board';
-    board.className = 'board';
 
-    for (var i = 0; i < height; i++) {
-      var row = document.createElement('div');
-      row.className = 'row';
-      row.style.display = 'block';
-      row.style.margin = '0px';
-      row.style.padding = '0px';
+var generateBoard = function(width, height, parent) {
+  width = width || 8;
+  height = height || 8;
+  parent = parent || document.body;
+  const TILE_DIM = 70;
 
-      
-      for (var j = 0; j < width; j++) {
-        var tile = document.createElement('div');
+  var board = document.createElement('div');
+  board.id = 'board';
+  board.className = 'board';
+  board.style.border = '1px solid black';
+  board.style.width = TILE_DIM * width + 2 + 'px';
+  board.style.height = TILE_DIM * height + 2 + 'px';
+  board.style.margin = '10px';
 
-        tile.className = 'tile';
-        tile.style.width = tile.style.height = '100px';
-        tile.style.backgroundColor = (i + j) % 2 === 0 ? '#F4F4F4' : '#A3A3A3';
-        tile.style.display = 'inline-block';
-        tile.style.border = '1px solid black';
-        tile.style.margin = '0px';
-        tile.style.padding = '0px';
+  for (var i = 0; i < height; i++) {
+    var row = document.createElement('div');
+    row.className = 'row';
+    row.style.display = 'block';
+    row.style.margin = '0px';
+    row.style.padding = '0px';
+    row.style.cssFloat = 'left';
+    row.style.clear = 'both';
 
-        row.appendChild(tile);
-      }
+    for (var j = 0; j < width; j++) {
+      var tile = document.createElement('div');
 
-      board.appendChild(row);
+      tile.className = 'tile';
+      tile.style.width = tile.style.height = TILE_DIM + 'px';
+      tile.style.backgroundColor = (i + j) % 2 === 0 ? '#F4F4F4' : '#A3A3A3';
+      tile.style.display = 'inline-block';
+      tile.style.border = '0px solid black';
+      tile.style.margin = '0px';
+      tile.style.padding = '0px';
+
+      row.style.height = tile.style.height;
+      row.style.width = TILE_DIM;
+
+      row.appendChild(tile);
     }
 
-    parent.appendChild(board);
+    // board.style.height = board.style.width = board.childNodes[0].style.width
 
-    return board;
+    board.appendChild(row);
+  }
+
+  parent.appendChild(board);
+
+  return board;
+};
+
+// get one piece to appear in one tile
+var placePieces = function (board) {
+  for (var i = 0; i < board.childNodes.length; i++) {
+    var row = board.childNodes[i];
+    for (var j = 0; j < board.childNodes.length; j++) {
+      var tile = row.childNodes[j];
+      var piece = document.createElement('span');
+      piece.style.fontSize = '40px';
+      piece.innerHTML = '&#9812;';
+      if (i === 0 && j === 4) tile.appendChild(piece);
+    }
   };
-
-  var board = generateBoard();
-  console.log(board);
-});
+};
+// create pieces from unicode
+  // set it up to redraw entire board from model rather than manipulating pieces on dom directly
